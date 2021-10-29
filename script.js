9002dcbfe9fb282589d0dfd40eb72c68c8c02b2f
zatchel <83443465+zatchel@users.noreply.github.com>
2021-10-29 18:33:35 -0500

    still working on layout, projects section

--- script.js
+++ script.js
@@ -1,7 +1,57 @@
 const toggleButton = document.getElementsByClassName('navbarToggle')[0];
 const navbarLinks = document.getElementsByClassName('navbarLinks');
 
+const showCodeButtonOne = document.getElementById('showCodeProjectOne');
+const codeSpanOne = document.getElementById('projectOneCode');
+
+const showCodeButtonTwo = document.getElementById('showCodeProjectTwo');
+const codeSpanTwo = document.getElementById('projectTwoCode');
+
+const showCodeButtonThree = document.getElementById('showCodeProjectThree');
+const codeSpanThree = document.getElementById('projectThreeCode');
+
+const showCodeButtonFour = document.getElementById('showCodeProjectFour');
+const codeSpanFour = document.getElementById('projectFourCode');
+
+
 toggleButton.addEventListener('click', function() {
     for(let i=0; i<navbarLinks.length; i++)
     navbarLinks[i].classList.toggle('active');
 })
+
+showCodeButtonOne.addEventListener('click', () => {
+    if (codeSpanOne.className == "projectOneCodeAfterClick"){
+        codeSpanOne.className = "projectOneCode";
+    }else{
+    codeSpanOne.className = "projectOneCodeAfterClick";
+    }
+})
+
+
+showCodeButtonTwo.addEventListener('click', () => {
+    if (codeSpanTwo.className == "projectTwoCodeAfterClick"){
+        codeSpanTwo.className = "projectTwoCode";
+    }else{
+    codeSpanTwo.className = "projectTwoCodeAfterClick";
+    }
+})
+
+
+showCodeButtonThree.addEventListener('click', () => {
+    if (codeSpanThree.className == "projectThreeCodeAfterClick"){
+        codeSpanThree.className = "projectThreeCode";
+    }else{
+    codeSpanThree.className = "projectThreeCodeAfterClick";
+    }
+})
+
+showCodeButtonFour.addEventListener('click', () => {
+    if (codeSpanFour.className == "projectFourCodeAfterClick"){
+        codeSpanFour.className = "projectFourCode";
+    }else{
+    codeSpanFour.className = "projectFourCodeAfterClick";
+    }
+})
+
+
+
